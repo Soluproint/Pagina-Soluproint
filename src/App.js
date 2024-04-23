d4cfacb9162e9aa52064e045c8f53390e1c65ee2
ManuelaLopezV <manuela.lopez@seguridadalamano.com>
2024-04-23 12:50:52 -0500

    Post Blog y Navbar sticky

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import './styles.css';
 import { BrowserRouter as Router, Routes, Route, useLinkClickHandler} from 'react-router-dom';
+
+//Importamos los estilos de las páginas
+import './styles.css';
+import './components/sections/AirConditioners/air-styles.css'
+import './components/sections/PostBlog/post-styles.css'
 
 //Importamos las vistas de las páginas
 import HomePage from './pages/HomePage';
@@ -7,16 +11,18 @@ import AboutUs from './pages/AboutUs';
 import ServicePortfolio from './pages/ServicePortfolio';
 import Cybersecurity from './pages/Cybersecurity';
 import Blog from './pages/Blog';
+import BlogStrapi from './pages/BlogStrapi';
 import Contact from './pages/Contact';
 
 //Importamos los componentes de las páginas
 import Navbar from './components/browsing/Navbar'
 import Footer from './components/browsing/Footer'
 import ServicesForm from './components/sections/ServicesForm';
-import AirConditioners from './components/sections//AirConditioners/AirConditioners';
+import AirConditioners from './components/sections/AirConditioners/AirConditioners';
 import SGSI from './components/sections/SGSI';
 import Infraestructure from './components/sections/Infraestructure';
 import ContactForm from './components/sections/ContactForm';
+import PostBlog from './components/sections/PostBlog/PostBlog';
 
 //Componente donde se va a construir toda la aplicación
 function App() {
@@ -24,22 +30,24 @@ function App() {
     <>
     <div className="App">
       <Router>
-        <header id="heading">
+        <header className="header" id="heading">
           <Navbar/>
-          <Routes>
-            <Route path='/' element={<HomePage/>}/>
-            <Route path='/aboutUs' element={<AboutUs/>}/>
-            <Route path='/servicePortfolio' element={<ServicePortfolio/>}/>
-            <Route path='/cybersecurity' element={<Cybersecurity/>}/>
-            <Route path='/blog' element={<Blog/>}/>
-            <Route path='/contact' element={<Contact/>}/>
-            <Route path='/servicesForm' element={<ServicesForm/>}/>
-            <Route path='/serviceConsulting' element={<SGSI/>}/>
-            <Route path='/serviceAir' element={<AirConditioners/>}/>
-            <Route path='/serviceInfraestructure' element={<Infraestructure/>}/>
-            <Route path='/contactForm' element={<ContactForm/>}/>
-          </Routes>
         </header>
+        <main id="main">
+          <Routes>
+              <Route path='/' element={<HomePage/>}/>
+              <Route path='/aboutUs' element={<AboutUs/>}/>
+              <Route path='/servicePortfolio' element={<ServicePortfolio/>}/>
+              <Route path='/cybersecurity' element={<Cybersecurity/>}/>
+              <Route path='/blog' element={<BlogStrapi/>}/>
+              <Route path='/contact' element={<Contact/>}/>
+              <Route path='/servicesForm' element={<ServicesForm/>}/>
+              <Route path='/serviceConsulting' element={<SGSI/>}/>
+              <Route path='/serviceAir' element={<AirConditioners/>}/>
+              <Route path='/serviceInfraestructure' element={<Infraestructure/>}/>
+              <Route path='/contactForm' element={<ContactForm/>}/>
+            </Routes>
+        </main>
         <footer id="end">
           <Footer/>
         </footer>
---
+++ src/pages/BlogStrapi.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+//Importamos los componentes de las páginas
+import SliderBlog from "../components/sections/SliderBlog";
+import PostBlog from "../components/sections/PostBlog/PostBlog";
+
+function BlogStrapi () {
+    return (
+        <div className="pages">
+            <main id="content">
+                <SliderBlog/>
+                <PostBlog/>
+            </main>
+        </div>
+
+    );
+}
+
+
+export default BlogStrapi;
